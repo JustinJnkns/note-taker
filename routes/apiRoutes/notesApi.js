85c6ee23844ec51fcb93f3711209870f426d3a28
JustinJnkns <69018193+JustinJnkns@users.noreply.github.com>
2020-10-26 05:11:44 -0400

    worked on notes api and /notes POST

--- routes/apiRoutes/notesApi.js
+++ routes/apiRoutes/notesApi.js
@@ -1,2 +1,20 @@
 const router = require('express').Router()
+const fs = require('fs')
+const { notes } = require("../../db/db.json")
+const { validation, noteCreation } = require('../../lib/notes')
+
+router.get("/notes", (req, res) => {
+    fs.readFile("./db/db.json", "utf8", function(err, contents) {
+        var results = JSON.parse(contents);
+        res.send(results);
+    });
+});
+
+
+router.post('/notes', (req, res) => {
+    console.log(req.body)
+    res.json(req.body)
+
+})
+
 module.exports = router
