83b36b58dd9aee3b57b2ad2b8f5a8b997ecc13ba
JustinJnkns <69018193+JustinJnkns@users.noreply.github.com>
2021-01-25 01:01:12 -0500

    notes displaying on right side when clicked

--- routes/apiRoutes/notesApi.js
+++ routes/apiRoutes/notesApi.js
@@ -1,17 +1,29 @@
 const router = require('express').Router()
 const fs = require('fs')
-const {validation,noteCreation} = require('../../lib/notes')
+const {validation,noteCreation,} = require('../../lib/notes')
 const {notes} = require('../../db/db.json')
 router.get("/notes", (req, res) => {
         res.json(notes)
     });
 
-
+router.get("/notes/:id", (req, res) => {
+        const result = findById(req.params.id, notes);
+        if (result) {
+          res.json(result);
+        } else {
+          res.send(404);
+        }
+      })
 
 router.post('/notes', (req, res) => {
+    // set id based off index in array
+    req.body.id = notes.length.toString()
+    if(!validation (req.body)){
+        res.status(400).send('note improperly formatted')
+    }else{
     const newNote = noteCreation(req.body,notes)
     res.json(newNote)
-
+    }
 })
 
 module.exports = router
